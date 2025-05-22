d9d0436bbeffe7e99a7bba1bff414c3c124f0a42
Ben <bmccall@greenville.k12.sc.us>
2025-05-22 15:13:51 -0400

    Checkpoint 2025-05-22_15-09-08

--- checkpoint.bat
+++ checkpoint.bat
@@ -3,8 +3,10 @@ REM Set the timestamp
 set dt=%date:~10,4%-%date:~4,2%-%date:~7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
 set dt=%dt: =0%
 
+REM Make sure checkpoints folder exists
+if not exist checkpoints mkdir checkpoints
+
 REM Optional: Create a zip archive of your project as a checkpoint
-REM (adjust folders/files as needed)
 powershell Compress-Archive -Path * -DestinationPath "checkpoints\checkpoint_%dt%.zip"
 
 REM Stage all changes
