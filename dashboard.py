5a7874c207d8664f9c879ff29ffbe2e97db205c0
Ben <bmccall@greenville.k12.sc.us>
2025-06-11 14:25:16 -0400

    Force bind to 0.0.0.0 and dynamic port

--- dashboard.py
+++ dashboard.py
@@ -483,7 +483,6 @@ def index():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    host = "0.0.0.0" if os.environ.get("PORT") else "127.0.0.1"
-    app.run(host=host, port=port, debug=True)
+    port = int(os.environ.get("PORT", "5000"))  # Always pulls a string first
+    app.run(host="0.0.0.0", port=port, debug=True)
 
