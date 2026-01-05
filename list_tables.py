49cb47ad6fc2db11944e1459e963aad0dcb7e1f3
Ben <bmccall@greenville.k12.sc.us>
2026-01-05 13:15:49 -0500

    Remove temporary list_tables helper

--- list_tables.py
+++
@@ -1,8 +0,0 @@
-﻿import sqlite3
-
-db = r"C:\TradeAlerts\live.db"
-con = sqlite3.connect(db)
-cur = con.cursor()
-rows = cur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name").fetchall()
-print(rows)
-con.close()
